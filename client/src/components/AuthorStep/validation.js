2479d2042bcba83490b1bbf2b5c51b59aa1dd850
Josh Goodman <jogoodma@iu.edu>
2019-10-02 17:17:31 -0400

    Added basic postgres config with schemas.

--- client/src/components/AuthorStep/validation.js
+++ client/src/components/AuthorStep/validation.js
@@ -2,6 +2,10 @@ import * as Yup from 'yup'
 
 export const AuthorSchema = Yup.object().shape({
   name: Yup.string().required('Name required'),
-  email: Yup.string().email('Invalid email').required('Email required'),
-  email_verify: Yup.string().email('Invalid email').required('Email required')
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email required'),
+  email_verify: Yup.string()
+    .email('Invalid email')
+    .required('Email required'),
 })
---
+++ db/initdb.d/01.schemas.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA ftyp;
+CREATE SCHEMA chado;
+
