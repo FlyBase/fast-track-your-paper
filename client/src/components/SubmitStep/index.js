7b2c95ad5fc6584092e0e6ea648085bcd4b42d3b
Jim Thurmond <jim.flybase@cox-thurmond.net>
2019-11-18 16:22:19 -0500

    start on Thanks page

--- client/src/components/SubmitStep/index.js
+++ client/src/components/SubmitStep/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const SubmitStep = () => <div>Submit step</div>
+const SubmitStep = () => (
+  <>
+    <div className="panel panel-primary">
+      <div className="panel-heading">Thank you!</div>
+      <div className="panel-body"></div>
+      <div className="panel-footer"></div>
+    </div>
+  </>
+)
 
 export default SubmitStep
