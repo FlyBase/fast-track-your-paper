e4a73994124c0406f1c33e8e147bf317dbe9eb67
Jim Thurmond <jim.flybase@cox-thurmond.net>
2019-11-21 16:38:42 -0500

    reworked handling of citation submission; hooked up buttons; some CSS class improvements

--- client/src/components/Citation/index.js
+++ client/src/components/Citation/index.js
@@ -1,49 +1,46 @@
 import React, { useRef } from 'react'
 import IconHelp from '../IconHelp'
 
-const Citation = () => {
-
-  const formEl = useRef(null)
+const Citation = ( { send } ) => {
   const textareaEl = useRef(null)
 
   const handleSubmit = e => {
-    const currEl = e.currentTarget
-    if (
-      (textareaEl.current === currEl && e.key === 'Enter') ||
-      formEl.current === e.target
-    ) {
-      send('CITATION', { citation: textareaEl.current.value } )
-      e.preventDefault()
-    }
+    console.log('handleSubmit fired');
+    send('CITATION.SUBMIT', { citation: textareaEl.current.value })
+    e.preventDefault()
   }
 
   return (
     <div className="col-xs-12">
-      <div className="panel panel-primary">
-        <div className="panel-heading">
-          <h3 className="panel-title">Enter Citation</h3>
-        </div>
-        <div className="panel-body">
-          <form className="form" ref={formEl} onSubmit={handleSubmit}>
-             <div className="form-group">
-               <textarea className="form-control" ref={textareaEl} ></textarea>
-               <IconHelp initial={false}>
-                 <br />
-                 Please uniquely identify the publication.  For example,
-                 enter a full citation with all bibliographic details included,
-                 a Digital Object Identifier (DOI) or a PubMed ID (PMID).
-               </IconHelp>
-             </div>
-             <div className="form-group">
-               <button>Search Pubs</button>
-               <button>Submit Citation</button>
-             </div>
+      <div className="well">
+
+          <form className="form-horizontal">
+            <div className="form-group">
+              <div className="col-sm-11">
+                <textarea className="form-control" ref={textareaEl} placeholder="enter citation"></textarea>
+              </div>
+
+              <IconHelp initial={false}>
+                <br /><br />
+                Please uniquely identify the publication. For example, enter a
+                full citation with all bibliographic details included, a Digital
+                Object Identifier (DOI) or a PubMed ID (PMID).
+              </IconHelp>
+
+            </div>
+            <div className="form-group">
+              <div className="col-sm-6">
+                <button className="btn btn-primary" onClick={() => send('GOTO.SEARCH')}>Search Pubs</button>
+              </div>
+              <div className="col-sm-5">
+                <button className="btn btn-primary pull-right" onClick={handleSubmit}>Submit Citation</button>
+              </div>
+            </div>
           </form>
-        </div>
+
       </div>
     </div>
   )
-
 }
 
 export default Citation
