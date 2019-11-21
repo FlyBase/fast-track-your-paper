3c661389d223254339f30a654fdb138b5f0f4349
Jim Thurmond <jim.flybase@cox-thurmond.net>
2019-11-21 14:09:56 -0500

    changes to handle curation status states

--- client/src/components/ChosenPub/index.js
+++ client/src/components/ChosenPub/index.js
@@ -2,25 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const ChosenPub = ({ pub }) => {
+  let cur_status_phrase_for = {
+    bibl: 'has not been curated',
+    skim: 'has been first-pass curated by FlyBase',
+    user: 'has been first-pass curated using the FTYP tool',
+    full: 'has been fully curated by FlyBase',
+  }
+
   return (
     <div style={{ paddingTop: '1rem' }}>
       <div className="row">
         <div className="col-sm-12">
           <div className="well small">
             <p>
-              <strong>The selected publication:</strong>
-              {pub.miniref}
+              <strong>The selected publication:</strong>&emsp;{pub.miniref}
+              <br />
+              {cur_status_phrase_for[pub.curationStatus]}.
             </p>
             <p>
-              <strong>Please note:</strong>
-              The Fast-Track Your Paper tool accesses our &ldquo;live&rdquo;
-              FlyBase references database in order to provide users with the
-              most up-to-date information. Data in this internal database is
-              3-12 weeks ahead of the data displayed on the current release of
-              the FlyBase website. This means that a paper that is{' '}
-              <i>unknown</i> or <i>uncurated</i> according to the FlyBase
-              website may actually already be known to FlyBase and/or show a
-              curated status when using the Fast-Track Your Paper tool.
+              <strong>Please note:</strong>&ensp; The Fast-Track Your Paper tool
+              accesses our &ldquo;live&rdquo; FlyBase references database in
+              order to provide users with the most up-to-date information. Data
+              in this internal database is 3-12 weeks ahead of the data
+              displayed on the current release of the FlyBase website. This
+              means that a paper that is <i>unknown</i> or <i>uncurated</i>{' '}
+              according to the FlyBase website may actually already be known to
+              FlyBase and/or show a curated status when using the Fast-Track
+              Your Paper tool.
             </p>
             <p>
               If you feel that there is an error, please{' '}
