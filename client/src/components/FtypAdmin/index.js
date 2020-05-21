9e713c2d0487a596af7727fdc0a329915a112148
Josh Goodman <jogoodma@iu.edu>
2020-05-21 16:35:18 -0400

    Working on admin section.
    * Added dev express table
    * Upgraded dependencies
    * Added ftyp_admin schema

---
+++ client/src/components/FtypAdmin/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import SubmissionTable from 'components/SubmissionTable'
+
+const FtypAdmin = () => (
+  <div>
+    <h3>FTYP Admin - Submissions</h3>
+    <SubmissionTable />
+  </div>
+)
+export default FtypAdmin
