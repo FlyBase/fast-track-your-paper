4b9e92bc230d6632df2aba8a64a3d64fa2254b57
Josh Goodman <jogoodma@iu.edu>
2020-07-13 22:50:53 -0400

    Missed filename change.  Added DB indices.

--- db/scripts/load_flags.sql
+++ db/scripts/load_flags.sql
@@ -6,4 +6,9 @@ CREATE TABLE ftyp_hidden.text_mining_flag
     flag_type text
 );
 
-COPY ftyp_hidden.text_mining_flag FROM '/ftyp/data/text_mining/textmining_positive_SVM.txt';
+COPY ftyp_hidden.text_mining_flag FROM '/ftyp/data/text_mining/data_flags.tsv';
+
+CREATE INDEX tf_epicycle_idx ON ftyp_hidden.text_mining_flag (epicycle);
+CREATE INDEX tf_fbrf_idx ON ftyp_hidden.text_mining_flag (fbrf);
+CREATE INDEX tf_data_type_idx ON ftyp_hidden.text_mining_flag (data_type);
+CREATE INDEX tf_flag_type_idx ON ftyp_hidden.text_mining_flag (flag_type);
