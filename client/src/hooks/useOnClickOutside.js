fbc1fb7538c4b35c662f56fdab23bd272259a314
Josh Goodman <jogoodma@iu.edu>
2020-08-02 12:05:17 -0400

    Added hook docs.

--- client/src/hooks/useOnClickOutside.js
+++ client/src/hooks/useOnClickOutside.js
@@ -1,6 +1,36 @@
 import { useEffect } from 'react'
 
+/**
+ * This hook takes a React reference (created by `useRef`)
+ * and a function handler and then executes the handler when
+ * the user touches/clicks outside of the element indicated
+ * by the ref. The handler function will receive the event
+ * object that triggered the execution.
+ *
+ * e.g.
+ *
+ * import { useRef } from 'react'
+ * import { useOnClickOutside } = './path/useOnClickOutside'
+ *
+ * const onClose = () => console.log("onClose triggered")
+ *
+ * const MyDiv = () => {
+ *   const ref = useRef()
+ *   useOnClickOutside(ref, onClose)
+ *
+ *   return (
+ *     <div ref={ref}>
+ *     </div>
+ *   )
+ * }
+ *
+ * Hook source - https://usehooks.com/useOnClickOutside/
+ *
+ * @param ref     - React reference for element to watch for clicks outside.
+ * @param handler - Function to execute when a click outside is registered.
+ */
 export const useOnClickOutside = (ref, handler) => {
+  // Execute the following when the reference or handler update.
   useEffect(
     () => {
       const listener = (event) => {
@@ -8,12 +38,15 @@ export const useOnClickOutside = (ref, handler) => {
         if (!ref.current || ref.current.contains(event.target)) {
           return
         }
+        // Run handler, passing down the event object.
         handler(event)
       }
+      // Attach listener to global document events.
       document.addEventListener('mousedown', listener)
       document.addEventListener('touchstart', listener)
 
       return () => {
+        // Remove event listeners when component is unmounted.
         document.removeEventListener('mousedown', listener)
         document.removeEventListener('touchstart', listener)
       }
