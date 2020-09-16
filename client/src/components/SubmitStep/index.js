9b010af781ca8d43aee7f193771fd22719ed0355
Jim Thurmond <jim.flybase@cox-thurmond.net>
2020-09-16 10:12:02 -0400

    new Gene Snapshot invitation panel

--- client/src/components/SubmitStep/index.js
+++ client/src/components/SubmitStep/index.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { loader } from 'graphql.macro'
+
+// The GraphQL query to fetch the submission
+const submissionQuery = loader('graphql/getSubmission.gql')
 
 const SubmitStep = ({ submission = {}, result, children }) => {
+  // not using any of these four constants yet
+  const { fbrf } = useParams()
+  const { loading, error, data } = useQuery(submissionQuery, {
+    variables: { fbrf },
+  })
+  console.log(submission);
+  let noSumGeneList = [];
+  for( let n=0; n<submission.genes.length; n++ ) {
+     if(!submission.genes[n].hasSummary) {
+        let gn = submission.genes[n];
+        let wufooURL = 'https://flybase.wufoo.com/forms/flybase-gene-snapshot-form';
+        wufooURL += '?Field37='+submission.contact.name;
+        wufooURL += '&Field4='+submission.contact.email;
+        wufooURL += '&Field31='+gn.id;
+        wufooURL += '&Field3='+gn.symbol;
+        noSumGeneList.push(<li><a href={encodeURI(wufooURL)} target="_blank"><b>{gn.symbol}</b> <i class="fa fa-external-link" /></a></li>)
+     }
+  }
+  console.log(noSumGeneList);
+  let sumInvite = (
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4>FlyBase Gene Snapshots</h4>
+      </div>
+      <div class="panel-body">
+        Based on the genes associated with your recent publication,
+        you've done work on some genes that do not currently have{' '}
+        <a href="https://wiki.flybase.org/wiki/Gene_Snapshots">Gene Snapshots</a>.
+        Would you like to contribute a Gene Snapshot for any or all of the following genes?
+        <ul>{ noSumGeneList }</ul>
+      </div>
+      <div class="panel-footer text-primary">
+        Links will open a new tab/window containing a form hosted at wufoo.com for FlyBase.
+      </div>
+    </div>
+  )
   return (
     <>
       {/** TODO: Add follow up statements based submission flags and genes. */}
       <h2>Thank you!</h2>
+      { (noSumGeneList.length) ? sumInvite : '' }
       {children}
     </>
   )
